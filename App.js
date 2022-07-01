49204968615614af6edcdd3ad80d7cbcaa4aad0a
Wladmir Bullem <103560468+Dev-Wlad@users.noreply.github.com>
2022-06-30 23:06:12 -0300

    Add files via upload
    Está versão e para que outras pessoas possam realizar o teste em seus emuladores.

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
+import style from './assets/style.js';
+
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
+
+  useEffect(() => {
+    // Ligar o flash do celular
+    Torch.switchState(toggle);
+  }, [toggle]);
+
+  useEffect(() => {
+    /**
+     * Quando o celular for chacoalhado, será alterado o toggle
+     */
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle);
+    });
+
+    // Essa func vai ser chamada quando o componente
+    // For desmontado
+    return () => subscription.remove();
+  }, []);
+
+  return (
+    <View style={toggle ? style.containerLight : style.container}>
+      <TouchableOpacity onPress={handleChangeToggle}>
+        <Image
+          style={toggle ? style.lightingOn : style.lightingOff}
+          source={
+            toggle
+              ? require('./assets/icons/eco-light.png')
+              : require('./assets/icons/eco-light-off.png')
+          }
+        />
+        <Image
+          style={style.dioLogo}
+          source={
+            toggle
+              ? require('./assets/icons/logo-dio.png')
+              : require('./assets/icons/logo-dio-white.png')
+          }
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default App;
